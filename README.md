02a3738000a404cb16a81bad61f0023356abac12
Episch <david@episch.org>
2017-09-10 02:04:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# brain subimage
+
+## install script
+```
+sudo apt-get install python python-pip
+```
+```
+sudo pip opencv-python
+```
+```
+git clone [REPO]
+```
+
+## run script
+```
+python findImage.py splatoon-2-salmon-run-times.jpg wep/
+```
+```
+python findImage.py
+```
+
+## result image
+![Result Image](result.png?raw=true "Result Image")
---
+++ findImage.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import sys
+import os
+
+
+baseImage = 'salmonscreen.jpg'
+if len(sys.argv) > 1:
+	baseImage = sys.argv[1]
+path = 'wep'
+if len(sys.argv) > 2:
+	path = sys.argv[2]
+img_rgb = cv2.imread(baseImage)
+for weapon in os.listdir(path):
+	template = cv2.imread(os.path.join(path, weapon))
+	w, h = template.shape[:-1]
+	res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+	threshold = 0.75
+	loc = np.where(res >= threshold)
+	print("---- "+weapon+"\n")
+	for pt in zip(*loc[::-1]):  # Switch collumns and rows
+		print(pt)
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+		cv2.imwrite('result.png', img_rgb)
+	print("Done!")
